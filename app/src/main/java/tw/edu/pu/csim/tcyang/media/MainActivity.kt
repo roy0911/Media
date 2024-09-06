33e0597ea90120df3a39df7006863cabab597aae
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-09-06 16:41:43 +0800

    v6_MediaPlayer

--- app/src/main/java/tw/edu/pu/csim/tcyang/media/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/media/MainActivity.kt
@@ -1,5 +1,6 @@
 package tw.edu.pu.csim.tcyang.media
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -22,6 +23,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,16 +56,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
     var AnimalsName = arrayListOf("鴨子","企鵝","青蛙","貓頭鷹","海豚", "牛", "無尾熊", "獅子", "狐狸", "小雞")
 
+    val context = LocalContext.current
+    var mper = MediaPlayer()
+
     Column {
         Row{
             Button(onClick = {
-                //your onclick code here
+                mper.reset()
+                mper = MediaPlayer.create(context, R.raw.tcyang)
+                mper.start()
             }) {
                 Text(text = "歡迎修課")
             }
 
             Button(onClick = {
-                //your onclick code here
+                mper.reset()
+                mper = MediaPlayer.create(context, R.raw.fly)
+                mper.start()
             }) {
                 Text(text = "展翅飛翔")
             }
